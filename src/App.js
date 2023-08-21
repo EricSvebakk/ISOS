087b6855425456941746266c4257943830898cb1
Tepohe <ericatsvebakk@gmail.com>
2023-08-21 02:13:50 +0200

    fixed subjectManager (still W.I.P)

--- src/App.js
+++ src/App.js
@@ -1,179 +1,35 @@
 
 import React, { Component } from "react";
-import data from "./data/ifi_subjects_010823.json";
-// import Subjects from "./components/subjectManagement/Subjects";
-// import SemesterPlan from "./components/Semesterplan";
-// import Settings from "./components/Settings";
-import SubjectManager from "./components/subjectManagement/SubjectManager";
 
+import data from "./data/ifi_subjects_010823.json";
+// import { codeColor, semesterColor } from "./components/NodeColor";
 
 import { MyNavbar } from "./components/Navbar";
-import { ForceGraph } from "./components/graphing/ForceGraph";
-import { codeColor, semesterColor } from "./components/NodeColor";
-// import { SubjectManager } from "./components/subjectManagement/SubjectManager";
+import SubjectManager from "./components/subjectManagement/SubjectManager";
+// import { ForceGraph } from "./components/graphing/ForceGraph";
+// import SemesterPlan from "./components/Semesterplan";
+
 
 class App extends Component {
+  
   state = {
+    subjectsAll: data.nodes,
     subjectsSelected: [],
-    nodesTopsorted: [],
-    nodesPosition: [],
-    semesters: [],
-    funcColor: codeColor,
-  };
-
-  // // ===============================================================
-  // // selects/deselects subjects to be show in force-graph
-  // toggleSubject = (e) => {
-    
-  //   console.log(e);
-    
-  //   const node = e.currentTarget.getAttribute("data-item");
-  //   const temp = data.nodes.filter((a) => a.code === node)[0];
-
-  //   const { subjectsSelected: selectedSubjects, forceGraphUpdater } = this.state;
-
-  //   // console.log("ts!", node)
-
-  //   if (!selectedSubjects.includes(temp)) {
-  //     const result = this.toggleSubjectRecursive([temp]);
-  //     const result2 = result.filter((n) => !selectedSubjects.includes(n));
-  //     const result3 = [...new Set(result2)];
-
-  //     let result4 = [...result3];
-  //     let equivs = null;
-
-  //     // console.log(result, result2, result3, result4);
-
-  //     // for (const temp_node of result3) {
-
-  //     //   // console.log("tn", temp_node.id, temp_node.equivalent)
-
-  //     //   if (temp_node.equivalent != null) {
-
-  //     //     let temp_equivs = [...temp_node.equivalent, temp_node.code];
-
-  //     //     equivs = data.nodes.filter((n) => temp_equivs.includes(n.code));
-  //     //     equivs = equivs.sort((a, b) => a.code.localeCompare(b.code));
-
-  //     //     let nodeUsed = equivs[0];
-
-  //     //     result4 = result4.filter((n) => !equivs.includes(n));
-  //     //     result4 = [...result4, nodeUsed]
-
-  //     //     console.log("er", equivs, result4);
-  //     //     // console.log(result4);
-
-  //     //     // console.log("NU", equivs, equivs[0]);
-
-  //     //     // result.filter((a) => {
-  //     //     //   for (const ntemp of equivs) {
-  //     //     //     if (a.code == ntemp.code) {
-  //     //     //       return false;
-  //     //     //     }
-  //     //     //   }
-  //     //     //   return true;
-  //     //     // });
-  //     //   }
-  //     // }
-
-  //     this.setState({
-  //       selectedSubjects: [...selectedSubjects, ...result4],
-  //     });
-  //   } else {
-  //     this.setState({
-  //       selectedSubjects: selectedSubjects.filter((sub) => sub !== temp),
-  //     });
-  //   }
-  //   forceGraphUpdater();
-  // };
-
-  // // helper-function for toggleSubject()
-  // toggleSubjectRecursive = (nodeList) => {
-  //   let result = [...nodeList];
-
-  //   for (const node of nodeList) {
-  //     // more code was here before
-  //     // nodeUsed used for overlap
-  //     let nodeUsed = node;
-
-  //     if (nodeUsed.msubs != null) {
-  //       let rec = data.nodes.filter((n) => nodeUsed.msubs.includes(n.id));
-  //       result = [...result, ...this.toggleSubjectRecursive(rec)];
-  //     }
-
-  //     if (nodeUsed.rsubs != null) {
-  //       let rec = data.nodes.filter((n) => nodeUsed.rsubs.includes(n.id));
-  //       result = [...result, ...this.toggleSubjectRecursive(rec)];
-  //     }
-
-  //     // if (equivs != null) {
-
-  //     //   // result = []
-  //     // }
-  //   }
-
-  //   return result;
-  // };
+    subjectsSelectedNodePosition: [],
+  }
+  
+  // new functions
+  // setSubjectsSelected = () => {
+  //   this.setState({
+  //     subjectsSelected
+  //   })
+  // }
 
   // ===============================================================
-  // deselects all subjects
-  clearSubjects = () => {
-    const { forceGraphUpdater } = this.state;
-
-    this.setState({
-      selectedSubjects: [],
-    });
-
-    forceGraphUpdater();
-  };
-
-  // ===============================================================
-  // sets the color-mode for the force-graph
-  setColorMode = (e) => {
-    const mode = e.currentTarget.getAttribute("data-item");
-    let temp;
-
-    const { forceGraphUpdater, funcColor } = this.state;
-
-    console.log(mode);
-
-    switch (mode) {
-      case "semester":
-        temp = semesterColor;
-        break;
-      // case "points":
-      //   temp = pointsColor;
-      //   break;
-      default:
-        temp = codeColor;
-        break;
-    }
-
-    if (temp !== funcColor) {
-      this.setState({
-        funcColor: temp,
-      });
-      forceGraphUpdater();
-    }
-  };
-
   // sets the function which must be called when updating forceGraph
   setForceGraphUpdater = (e) => {
     this.setState({
       forceGraphUpdater: e,
-    });
-  };
-
-  //
-  setTopsort = (e) => {
-    this.setState({
-      nodesTopsorted: e,
-    });
-  };
-
-  setSemesters = (e) => {
-    this.setState({
-      semesters: e,
     });
   };
 
@@ -190,20 +46,14 @@ class App extends Component {
 
   render() {
     const {
-      // subjectsSelected: selectedSubjects,
-      // visibleSubjects,
-      // sortingCategory,
-      // nodesTopsorted,
-      nodesPosition,
-      // semesters,
-      // filterSelected,
-      funcColor,
-      forceGraphUpdater,
+      subjectsAll
     } = this.state;
+    
 
     return (
       <div className="App">
         <MyNavbar></MyNavbar>
+        
         <div className="app-container">
           <section className="left">
             {/* <ForceGraph
@@ -215,54 +65,15 @@ class App extends Component {
               setNodesPosition={this.setNodesPosition}
               nodesPosition={nodesPosition}
             /> */}
-            {/* <div className="spacer" /> */}
-            {/* <div style={{ display: "inline-block" }}>
-              
-              <button
-                className="subject-button"
-                data-item={"code"}
-                onClick={this.setColorMode}
-              >
-                code
-              </button>
-              
-              <button
-                className="subject-button"
-                data-item={"semester"}
-                onClick={this.setColorMode}
-              >
-                semester
-              </button>
-              
-            </div> */}
           </section>
 
           <section className="middle">
             <SubjectManager
-              subjects={data.nodes}
-              setSubjects={null}
-              
-              forceGraphUpdater={forceGraphUpdater}
+              subjectsAll={subjectsAll}
             />
-            {/* <Subjects
-              nodesData={data.nodes}
-              // subjectsSelected={selectedSubjects}
-              // levelSelected={selectedSubjects}
-              // sortBy={sortingCategory}
-              // selectSubject={this.toggleSubject}
-              // funcSort={this.chooseSort}
-              // clearSubjects={this.clearSubjects}
-              // searchInSubjects={this.searchInSubjects}
-              // test={this.testing}
-              forceGraphUpdater={forceGraphUpdater}
-            /> */}
           </section>
 
           <section className="right">
-            {/* <Settings
-              filterFunction={this.filterSubjects}
-              filterSelected={filterSelected}
-            /> */}
             {/* <SemesterPlan
               nodesData={selectedSubjects}
               linksData={data.links}
--- src/components/subjectManagement/SubjectManager.js
+++ src/components/subjectManagement/SubjectManager.js
@@ -4,25 +4,80 @@ import React, { Component } from "react";
 
 class SubjectManager extends Component {
   
-  selectSubject = (e) => {};
-  selectPrereq = (e) => {};
-  clearSubject = (e) => {};
+  selectSubject = (e) => {  
+    const {
+      subjectsVisible,
+      subjectsSelected,
+    } = this.state;
+    
+    const dataItem = e.currentTarget.getAttribute("data-item");
+    const temp = subjectsVisible.filter((a) => a.code === dataItem)[0];
+
+    if (!subjectsSelected.includes(temp)) {
+      const result = this.selectPrereq([temp]);
+      const result2 = result.filter((n) => !subjectsSelected.includes(n));
+      const result3 = [...new Set(result2)];
+
+      let result4 = [...result3];
+      
+      // console.log("r4", result4)
+
+      this.setState({
+        subjectsSelected: [...subjectsSelected, ...result4],
+      });
+    }
+    
+    else {
+      this.setState({
+        subjectsSelected: subjectsSelected.filter((sub) => sub !== temp),
+      });
+    }
+    
+  };
+  
+  selectPrereq = (nodeList) => {
+    const {
+      subjectsAll
+    } = this.state;
+
+    let result = [...nodeList];
+    
+    for (const node of nodeList) {
+      let nodeUsed = node;
+      
+      if (nodeUsed.msubs != null) {
+        let rec = subjectsAll.filter((n) => nodeUsed.msubs.includes(n.id));
+        result = [...result, ...this.selectPrereq(rec)];
+      }
+      
+      if (nodeUsed.rsubs != null) {
+        let rec = subjectsAll.filter((n) => nodeUsed.rsubs.includes(n.id));
+        result = [...result, ...this.selectPrereq(rec)];
+      }
+    }
+    
+    
+    // console.log("res", result)
+    return result;
+  };
+  clearSubjects = () => {
+    this.setState({
+      subjectsSelected: [],
+    });
+  };
   
   
   constructor(props) {
     super(props);
-
-    // props:
-    // nodes
-    // 
     
     this.state = {
-      subjects: props.subjects,
+      subjectsAll: props.subjectsAll,
+      subjectsVisible: props.subjectsAll,
+      subjectsSelected: [],
 
-      subjectsVisible: props.subjects,
-      setSubjectsVisible: null,
-
-      setSubjectsSelected: null,
+      
+      // setSubjectsVisible: null,
+      // setSubjectsSelected: null,
 
       forceGraphUpdater: null,
     };
@@ -31,27 +86,90 @@ class SubjectManager extends Component {
   render() {
     
     const {
-      
-    } = this.props;
+      subjectsAll,
+      subjectsVisible,
+      subjectsSelected,
+    } = this.state;
     
+    // const {
+    //   subjectsAll,
+    // } = this.props;
     
-  	const theadData = null;
-    const tbodyData = null;
+    // console.log("ren", subjectsAll);
+    // console.log("ren2", subjectsAll);
+    
+    const c1 = { width: "10%" };
+    const c2 = { width: "40%" };
+    const c3 = { width: "15%" };
+    const c4 = { width: "10%" };
+    const c5 = { width: "15%" };
+    
+  	const theadData = <tr>
+      <th style={c1} data-item={"code"} id="th1">code</th>
+      <th style={c2} data-item={"title"}>title</th>
+      <th style={c4} data-item={"points"}>points</th>
+      <th style={c3} data-item={"semester"}>semester</th>
+      <th style={c5} data-item={"priority"}>priority</th>
+    </tr>
+    
+    const tbodyData = subjectsAll.map((row) => {
+    
+      const isSelected = subjectsSelected.map((d) => d.id).includes(row.id);
+      const semRow = row.semester === null ? 
+        <td style={c3} data-item={row.code}>{"utilgjengelig"}</td> :
+        <td style={c3} data-item={row.code}>{row.semester}</td>
+
+      return (
+        <tr key={row.id} style={trStyle(isSelected, row)}>
+          <td style={c1}>
+            <a style={{ color: "black" }} href={link(row.code)} target="_blank" rel="noopener noreferrer">
+              {row.code}
+            </a>
+          </td>
+          <td style={c2} data-item={row.code} onClick={this.selectSubject}>{row.title}</td>
+          <td style={c4} data-item={row.code} onClick={this.selectSubject}>{row.points}</td>
+          {semRow}
+          <td style={c5} data-item={row.code} onClick={this.selectSubject}>{row.requires != null ? row.requires.join(", ") : ""}</td>
+        </tr>
+      );
+    });
 
     // elements
     return (
-      <>
-        <div>
-          {/* search */}
-          <div></div>
-
-          {/* table */}
-          <table>
-            <thead>{theadData}</thead>
-            <tbody>{tbodyData}</tbody>
-          </table>
+      <div className="outer-outer">
+        <div className="subject-search">
+          <input
+            id="filter"
+            name="filter"
+            type="text"
+            placeholder="tast inn kode eller navn"
+            onChange={(event) => this.filterSubjects(event.target.value)}
+          />
+          <button className="...">search</button>
         </div>
-      </>
+
+        <div className="inner-container-subject-table">
+          <div>
+            {/* search */}
+            <div></div>
+
+            {/* table */}
+            <table className="subject-table">
+              <thead>{theadData}</thead>
+              <tbody>{tbodyData}</tbody>
+              <tfoot>
+                <button
+                  className="subject-button"
+                  // data-item={visibleSubjects}
+                  onClick={this.clearSubjects}
+                >
+                  nullstill
+                </button>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+      </div>
     );
   }
 }
@@ -59,49 +177,11 @@ class SubjectManager extends Component {
 export default SubjectManager;
 
 
-function selectSubject(nodeElement, subsVisible, subsSelected, FGUpdater) {
-
-  const node = nodeElement.currentTarget.getAttribute("data-item");
-  const temp = subsVisible.filter((a) => a.code === node)[0];
-
-  if (!subsSelected.includes(temp)) {
-    const result = selectPrereq([temp]);
-    const result2 = result.filter((n) => !subsSelected.includes(n));
-    const result3 = [...new Set(result2)];
-
-    let result4 = [...result3];
-
-	return [...subsSelected, ...result4]	
-  }
-  
-  else {
-	return subsSelected.filter((sub) => sub !== temp)
-  }
-  
-  FGUpdater();
-}
-
-// helper-function for selectSubject()
-function selectPrereq(nodeList) {
-  const { allNodes } = this.state;
-
-  let result = [...nodeList];
-
-  for (const node of nodeList) {
-
-    let nodeUsed = node;
-
-    if (nodeUsed.msubs != null) {
-      let rec = allNodes.filter((n) => nodeUsed.msubs.includes(n.id));
-      result = [...result, ...this.toggleSubjectRecursive(rec)];
-    }
-
-    if (nodeUsed.rsubs != null) {
-      let rec = allNodes.filter((n) => nodeUsed.rsubs.includes(n.id));
-      result = [...result, ...this.toggleSubjectRecursive(rec)];
-    }
-
-  }
-
-  return result;
+const trStyle = (pred, sub) => {
+  return {
+    // backgroundColor: c,
+    filter: "brightness(" + (pred ? "80" : "100") + "%)",
+  };
 };
+
+const link = (code) => "https://www.uio.no/studier/emner/matnat/ifi/" + code + "/index.html";
