943311193a80d59d9b5d5259cf24bb6ddffa4deb
Tepohe <ericatsvebakk@gmail.com>
2023-10-03 03:05:37 +0200

    Updates

---
+++ precache-manifest.aa9bb013f1ff8515dcc37239ff7b9216.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "32ab1246cf396b24b7b9",
+    "url": "/ISOS/static/css/main.ad18347b.chunk.css"
+  },
+  {
+    "revision": "32ab1246cf396b24b7b9",
+    "url": "/ISOS/static/js/main.32ab1246.chunk.js"
+  },
+  {
+    "revision": "7b257f46be129559b84b",
+    "url": "/ISOS/static/css/1.d5bc56bd.chunk.css"
+  },
+  {
+    "revision": "7b257f46be129559b84b",
+    "url": "/ISOS/static/js/1.7b257f46.chunk.js"
+  },
+  {
+    "revision": "c1a0861e20e6c114cb30",
+    "url": "/ISOS/static/js/runtime~main.c1a0861e.js"
+  },
+  {
+    "revision": "e422401ac985bbe3d8a83907959a2331",
+    "url": "/ISOS/static/media/BW_UiO_logo.e422401a.png"
+  },
+  {
+    "revision": "b87823f5dabf02964df790f272c27fd3",
+    "url": "/ISOS/index.html"
+  }
+];
